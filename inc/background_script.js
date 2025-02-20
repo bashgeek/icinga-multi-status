1ea0ded50118b8d92ea99c3d61bde444c82f6f99
Daniel <daniel@blendbyte.com>
2025-02-20 12:26:15 +0800

    wip

--- inc/background_script.js
+++ inc/background_script.js
@@ -1,1 +1,11 @@
 bg.init();
+
+browser.runtime.onStartup.addListener(() => {
+    bg.init();
+});
+
+browser.runtime.onInstalled.addListener(details => {
+    if (details.reason === 'install' || details.reason === 'update') {
+        bg.init();
+    }
+});
